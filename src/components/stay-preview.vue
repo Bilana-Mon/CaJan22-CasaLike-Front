f7eb3e179ca02718780df5e1bd8d1f1201a78bc5
Shunit Mizrachi <shuna1800@gmail.com>
2022-03-23 23:46:57 +0200

    add swiper

--- src/components/stay-preview.vue
+++ src/components/stay-preview.vue
@@ -1,58 +1,68 @@
 <template>
   <li class="stay-preview-container">
     <custom-card>
-      <template #header>
-        <img v-for="i in stay.imgUrls" :key="i" :src="`src/assets/imgs/stays/${i}`" />
-      </template>
+    <template #header>
+    <swiper :options="swiperOption" ref="swiper" class="swiper">
+      <swiper-slide v-for="i in stay.imgUrls" :key="i">
+        <button @click="$refs.swiper.swiperInstance.slidePrev()" class="swiper-button-prev">swipe my ass</button>
+        <img :src="`src/assets/imgs/stays/${i}`" />
+        <button @click="$refs.swiper.swiperInstance.slideNext()" class="swiper-button-next">swipe my ass</button>
+      </swiper-slide>  
+    </swiper>
+    </template>
 
-      <template #footer>
-        <p>
-          {{ getFormattedRate }}(
-          <span>{{ stay.numOfReviews }}</span>)
-        </p>
-        <p>
-          <span>{{ stay.roomType }}</span>·
-          <span>{{ getLocation }}</span>
-        </p>
-        <p>
-          <span>{{ stay.name }}</span>
-        </p>
-        <p>
-          <span>{{ getFormattedPrice }}</span>
-          <span>total</span>
-        </p>
-        <p>
-          <a href="#">Price breakdown</a>
-        </p>
-      </template>
+    <template #footer>
+    <p>
+      {{ getFormattedRate }}(
+      <span>{{ stay.numOfReviews }}</span>)
+    </p>
+    <p>
+      <span>{{ stay.roomType }}</span>·
+      <span>{{ getLocation }}</span>
+    </p>
+    <p>
+      <span>{{ stay.name }}</span>
+    </p>
+    <p>
+      <span>{{ getFormattedPrice }}</span>
+      <span>total</span>
+    </p>
+    <p>
+      <a href="#">Price breakdown</a>
+    </p>
+    </template>
     </custom-card>
   </li>
 </template>
 
 <script>
 import customCard from './custom-card.vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+
 
 export default {
   name: 'stay-preview',
   props: {
     stay: Object,
   },
-
-
   components: {
     customCard,
-
-
-  },
-  created() {
-
+    Swiper,
+    SwiperSlide
   },
   data() {
     return {
+      swiperOption: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }
+      }
     }
-  },
-  methods: {
-
   },
   computed: {
     getFormattedRate() {
@@ -68,11 +78,8 @@ export default {
     },
     getFormattedPrice() {
       return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(this.stay.price)
-    },
-
+    }
   },
-
-  unmounted() { },
 }
 </script>
 <style>

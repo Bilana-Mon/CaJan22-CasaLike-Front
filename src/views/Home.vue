174cd14a74379143f9fd9e37d089229d478f7dca
Kyrill Belenkov <bel.kir89@gmail.com>
2022-03-23 14:56:26 +0200

    added media content,imgs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,37 @@
 <template>
-  <section class="home-page">
+  <section class="home-page layout">
+    <div class="homepage-img">
+      <img alt="homepage-img" src="~@/assets/imgs/homepage/homepage.jpg">
+      <div class="jingle">
+      <h2>Where would you like to feel at home today?</h2>
+    </div>
+
+    <h1>Ultimate Rentals</h1>
+    <div class="rental-cards">
+
+      <div class="rental-card">
+        <h2>Copenhagen</h2>
+        <img class="rental-img" alt="copenhagen" src="~@/assets/imgs/homepage/kbh.webp">
+      </div>
+
+      <div class="rental-card">
+        <h2>Paris</h2>
+        <img class="rental-img" alt="paris" src="~@/assets/imgs/homepage/paris.webp">
+      </div>
+
+      <div class="rental-card">
+        <h2>Hongkong</h2>
+        <img class="rental-img" alt="hongkong" src="~@/assets/imgs/homepage/hongkong.webp">
+      </div>
+
+      <div class="rental-card">
+        <h2>San-Francisco</h2>
+        <img class="rental-img" alt="sanfrancisco" src="~@/assets/imgs/homepage/sanfran.webp">
+      </div>
+
+    </div>
+
+    </div>
   
   </section>
 </template>

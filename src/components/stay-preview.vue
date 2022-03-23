6c6f6098ffb925d1940ccc4647fa5ab8a687c0f5
Shunit Mizrachi <shuna1800@gmail.com>
2022-03-23 19:17:13 +0200

    changed stay preview

--- src/components/stay-preview.vue
+++ src/components/stay-preview.vue
@@ -1,46 +1,54 @@
 <template>
-
     <li class="stay-preview-container">
-<custom-card>
-     <template #header>
-         <img :src="getImgUrl" >
-     </template>
-      <template #footer>
-         <p><span>Name:</span> {{ stay.name }}</p>
-      </template>
-</custom-card>
+      <custom-card>
+
+        <template #header>
+          <img v-for="i in stay.imgUrls" :key='i' :src="`src/assets/imgs/stays/${i}`" >       
+        </template>
+
+        <template #footer>
+          <p><span>Name:</span> {{ stay.name }}</p>
+        </template>
+
+      </custom-card>
     </li>
-
-
 </template>
 
 <script>
-
 import customCard from './custom-card.vue'
+
 export default {
     name:'stay-preview',
  props: {
         stay: Object,
     },
+
+    
   components: {
-      customCard
+      customCard,
+ 
+
   },
   created() {
 
   },
   data() {
     return {
-
     }
   },
   methods: {
     
   },
   computed: {
-        getImgUrl() {
-          return `src/assets/imgs/stays/${this.stay.imgUrls[0]}`
-       }
+   
   },
+ 
   unmounted() {},
 }
 </script>
+<style>
+.stay-preview-container img {
+  width: 300px; 
+  height: 200px;
+}
+</style>

7cf3b79d50762e8cfbbd89945a2a6a4384854675
Bilana-Mon <bilanamoon@gmail.com>
2022-03-23 16:14:43 +0200

    fix

--- src/store/modules/order-module.js
+++ src/store/modules/order-module.js
@@ -1,40 +1,40 @@
-import { orderService } from "../../services/order.service.js"
+// import { orderService } from "../../services/order.service.js"
 
 export default {
-    state: {
-        orders: []
-    },
-    getters: {
-        orders({ orders }) {
-            return orders
-        }
-    },
-    mutations: {
-        setOrders(state, { orders }) {
-            state.orders = orders
-        },
-        saveOrder(state, { order }) {
-            const idx = state.orders.findIndex((currOrder) => currOrder._id === order._id)
-            if (idx !== -1) state.orders.splice(idx, 1, order)
-            else state.orders.push(order)
-        },
-        removeOrder(state, { orderId }) {
-            const idx = state.orders.findIndex((currOrder) => currOrder._id === orderId)
-            state.orders.splice(idx, 1)
-        }
-    },
-    actions: {
-        async loadOrders({ commit, state }) {
-            await orderService.query()
-            commit({ type: 'setOrders', orders })
-        },
-        async saveOrder({ commit }, { order }) {
-            await orderService.save(order)
-            commit({ type: 'saveOrder', order })
-        },
-        async removeOrder({ commit }, { orderId }) {
-            await orderService.remove(orderId)
-            commit({ type: 'removeOrder', orderId })
-        }
-    }
+    // state: {
+    //     orders: []
+    // },
+    // getters: {
+    //     orders({ orders }) {
+    //         return orders
+    //     }
+    // },
+    // mutations: {
+    //     setOrders(state, { orders }) {
+    //         state.orders = orders
+    //     },
+    //     saveOrder(state, { order }) {
+    //         const idx = state.orders.findIndex((currOrder) => currOrder._id === order._id)
+    //         if (idx !== -1) state.orders.splice(idx, 1, order)
+    //         else state.orders.push(order)
+    //     },
+    //     removeOrder(state, { orderId }) {
+    //         const idx = state.orders.findIndex((currOrder) => currOrder._id === orderId)
+    //         state.orders.splice(idx, 1)
+    //     }
+    // },
+    // actions: {
+    //     async loadOrders({ commit, state }) {
+    //         await orderService.query()
+    //         commit({ type: 'setOrders', orders })
+    //     },
+    //     async saveOrder({ commit }, { order }) {
+    //         await orderService.save(order)
+    //         commit({ type: 'saveOrder', order })
+    //     },
+    //     async removeOrder({ commit }, { orderId }) {
+    //         await orderService.remove(orderId)
+    //         commit({ type: 'removeOrder', orderId })
+    //     }
+    // }
 }

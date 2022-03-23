b3af64d16d6bad1312893ef23ec1b9876a70a22f
Bilana-Mon <bilanamoon@gmail.com>
2022-03-23 20:20:24 +0200

    fixed order service and module

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,71 +1,72 @@
-// import { utilService } from "./util.service.js";
-// import { storageService } from "./async-storage.service.js";
+import { utilService } from "./util.service.js";
+import { storageService } from "./async-storage.service.js";
 
 
-// const ORDERS_KEY = 'ordersDB'
+const ORDERS_KEY = 'ordersDB'
 // import gUsers from '../../data/user.json'
 // import gStays from '../../data/stay.json'
 
 export const orderService = {
-    // query,
-    // getById,
-    // remove,
-    // save,
-    // getEmptyOrder
+    query,
+    getById,
+    remove,
+    save,
+    getEmptyOrder
 }
-// const gOrders = _createOrders()
 
-// async function query() {
-//     return await storageService.query(ORDERS_KEY)
-// }
+_createOrders()
 
-// async function getById(id) {
-//     return await storageService.getById(ORDERS_KEY, id)
-// }
+async function query() {
+    return await storageService.query(ORDERS_KEY)
+}
 
-// function remove(id) {
-//     return storageService.delete(ORDERS_KEY, id)
-// }
+async function getById(id) {
+    return await storageService.getById(ORDERS_KEY, id)
+}
 
-// async function save(order) {
-//     return order._id
-//         ? await storageService.put(ORDERS_KEY, order)
-//         : await storageService.post(ORDERS_KEY, order)
-// }
+async function remove(id) {
+    return await storageService.delete(ORDERS_KEY, id)
+}
 
-// function getEmptyOrder() {
-//     return Promise.resolve({
-//         location: '',
-//         fromDate: new Date(),
-//         toDate: new Date(),
-//         capacity: null,
-//         user: '',
-//         host: '',
-//         createdAt: new Date()
-//     })
-// }
+async function save(order) {
+    return order._id
+        ? await storageService.put(ORDERS_KEY, order)
+        : await storageService.post(ORDERS_KEY, order)
+}
 
-// function _createOrders() {
-//     let orders = utilService.loadFromStorage(ORDERS_KEY)
-//     if (!orders || !orders.length) {
-//         orders = [
-//             _createOrder('Canada', new Date(2022, 2, 23), new Date(2022, 2, 25), 4, 'Edgar', 'Maria')
-//         ]
-//         utilService.saveToStorage(ORDERS_KEY, orders)
-//     }
-//     return orders
-// }
+function getEmptyOrder() {
+    return Promise.resolve({
+        location: '',
+        fromDate: new Date(),
+        toDate: new Date(),
+        capacity: null,
+        user: '',
+        host: '',
+        createdAt: new Date()
+    })
+}
 
-// function _createOrder(location, fromDate, toDate, capacity, user, host) {
-//     return {
-//         // _id: utilService.makeId(),
-//         location,
-//         fromDate,
-//         toDate,
-//         capacity,
-//         user: gUsers.fullname,
-//         host: gStays.host.fullname,
-//         createdAt: new Date()
-//     }
-// }
+function _createOrders() {
+    let orders = utilService.loadFromStorage(ORDERS_KEY)
+    if (!orders || !orders.length) {
+        orders = [
+            _createOrder('Canada', new Date(2022, 2, 23), new Date(2022, 2, 25), 4, 'Edgar', 'Maria')
+        ]
+        utilService.saveToStorage(ORDERS_KEY, orders)
+    }
+    return orders
+}
+
+function _createOrder(location, fromDate, toDate, capacity, user, host) {
+    return {
+        // _id: utilService.makeId(),
+        location,
+        fromDate,
+        toDate,
+        capacity,
+        user,
+        host,
+        createdAt: new Date()
+    }
+}
 

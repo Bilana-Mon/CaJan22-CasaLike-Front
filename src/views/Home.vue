588fca5b71015535487a7e993377ea9e31b60861
Kyrill Belenkov <bel.kir89@gmail.com>
2022-03-23 21:04:27 +0200

    added basic style on hmpage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,32 @@
 <template>
-  <section class="home-page layout">
+  <section class="homepage layout">
     <div class="homepage-img">
       <img alt="homepage-img" src="~@/assets/imgs/homepage/homepage.jpg">
       <div class="jingle">
-      <h2>Where would you like to feel at home today?</h2>
+      <p>Where would you like to feel at home today?</p>
     </div>
 
     <h1>Ultimate Rentals</h1>
     <div class="rental-cards">
 
       <div class="rental-card" @click="goExplore()">
-        <h2>Copenhagen</h2>
+        <p>Copenhagen</p>
         <img class="rental-img" alt="copenhagen" src="~@/assets/imgs/homepage/kbh.webp">
     
       </div>
 
       <div class="rental-card" @click="goExplore()">
-        <h2>Paris</h2>
+        <p>Paris</p>
         <img class="rental-img" alt="paris" src="~@/assets/imgs/homepage/paris.webp">
       </div>
 
       <div class="rental-card" @click="goExplore()">
-        <h2>Hongkong</h2>
+        <p>Hongkong</p>
         <img class="rental-img" alt="hongkong" src="~@/assets/imgs/homepage/hongkong.webp">
       </div>
 
       <div class="rental-card" @click="goExplore()">
-        <h2>San-Francisco</h2>
+        <p>San-Francisco</p>
         <img class="rental-img" alt="sanfrancisco" src="~@/assets/imgs/homepage/sanfran.webp">
       </div>
 
@@ -57,3 +57,45 @@ export default {
   unmounted() { },
 }
 </script>
+<style>
+
+
+body {
+    margin: 0;
+}
+img{
+  height: 100vh;
+  position: relative;
+}
+.rental-img {
+ width:300px;
+ height:300px;
+ border-radius: 10px;
+}
+
+.rental-cards {
+  cursor:pointer;
+  display: grid;
+  column-gap:20px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+
+}
+
+.rental-card {
+  border-radius: 10px;
+  object-fit: cover;
+}
+
+h2 {
+  text-transform: capitalize;
+  font-size: 22px;
+  
+}
+.jingle{
+    font-size: 30px;
+    text-align: center;
+    color: black;
+    position: absolute;
+    transform: translate( 10%,-350% );
+}
+</style>

bb6062ecfc90cfbdd0c9b1b316021f2494e584c7
Kyrill Belenkov <bel.kir89@gmail.com>
2022-03-24 15:25:46 +0200

    minor ui stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="homepage-img">
       <img alt="homepage-img" src="src/assets/imgs/homepage/homepage.jpg" />
       <div class="jingle">
-        <p>Where would you like to feel at home today?</p>
+        <h3>Where would you like to feel at home today?</h3>
       </div>
 
       <h1>Ultimate Destinations</h1>
@@ -54,40 +54,5 @@ export default {
 </script>
 <style>
 
-img {
-  height: 100vh;
-  position: relative;
-}
-.destination-img {
-  width: 300px;
-  height: 300px;
-  border-radius: 10px;
-}
 
-.destination-cards {
-  cursor: pointer;
-  display: grid;
-  column-gap: 30px;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-}
-
-.destination-card {
-  border-radius: 10px;
-  object-fit: cover;
-}
-
-.jingle {
-    font-family: airbnb-extrabold;
-    font-size: 40px;
-    text-align: center;
-    color: white;
-    position: absolute;
-    transform: translate(10%, -360%);
-    
-}
-
-svg {
-    height:20px;
-    width:20px;
-}
 </style>

e67c8ab094c72e402f3700c566384eef63bae76a
Bilana-Mon <bilanamoon@gmail.com>
2022-03-27 23:16:38 +0300

    cleared unused ui libraries from main js

--- src/main.js
+++ src/main.js
@@ -5,9 +5,8 @@ import store from './store'
 import './styles/styles.scss'
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css'
-import Datepicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
-import  VueGoogleMaps from '@fawmi/vue-google-maps'
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+
 
 const app = createApp(App)
 
@@ -15,7 +14,6 @@ const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(ElementPlus)
-app.component('Datepicker', Datepicker);
 app.use(VueGoogleMaps, {
     load: {
         key: '',

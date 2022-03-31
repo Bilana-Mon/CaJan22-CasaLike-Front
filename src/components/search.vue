8e10bfc7acb88230cc2a4dd9ef0564b44a683eee
Shunit Mizrachi <shuna1800@gmail.com>
2022-03-31 18:41:22 +0300

    fix style

--- src/components/search.vue
+++ src/components/search.vue
@@ -170,7 +170,6 @@
 </template>
 
 
-<!-- import { ref } from 'vue' -->
 <script>
 
 export default {
@@ -193,8 +192,6 @@ export default {
             isActive: true,
             selectOpen: false,
             searchExpanded: false
-
-
         }
     },
     created() {
@@ -244,26 +241,3 @@ export default {
 }
 </script>
 
-<style scoped>
-.demo-date-picker {
-    display: flex;
-    width: 100%;
-    padding: 0;
-    flex-wrap: wrap;
-}
-.demo-date-picker .block {
-    padding: 30px 0;
-    text-align: center;
-    border-right: solid 1px var(--el-border-color);
-    flex: 1;
-}
-.demo-date-picker .block:last-child {
-    border-right: none;
-}
-.demo-date-picker .demonstration {
-    display: block;
-    color: var(--el-text-color-secondary);
-    font-size: 14px;
-    margin-bottom: 20px;
-}
-</style>

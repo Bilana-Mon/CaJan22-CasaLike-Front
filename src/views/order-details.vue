9c31b4718338d4e816113e5891e7245ccdc2128a
Bilana-Mon <bilanamoon@gmail.com>
2022-04-01 23:24:24 +0300

    added style to pending reserve

--- src/views/order-details.vue
+++ src/views/order-details.vue
@@ -1,118 +1,154 @@
 <template>
-    <section class="order-details-container" v-if="order && isApproved">
-        <div class="order-details-success">
-            <h1>Your Order Details</h1>
-            <p>We thank you, for your reservation! Here are the details about your order:</p>
-            <div class="order-details-desc">
-                <div class="stay-order-details">
-                    <span>Name of the stay:</span>
-                    <span>{{ order.nameOfStay }}</span>
-                    <span>Address of the stay:</span>
-                    <span>{{ order.location }}</span>
-                    <span>The host:</span>
-                    <span>{{ order.host }}</span>
-                </div>
-                <hr class="hr" />
-                <span>From:</span>
-                <span>{{ getFormattedDate(order.dates['0']) }}</span>
-                <span>To:</span>
-                <span>{{ getFormattedDate(order.dates['1']) }}</span>
-                <hr class="hr" />
-                <div class="guests-order-details">
-                    <span>Number of guests:</span>
-                    <span>Adults:</span>
-                    <span>{{ order.capacity.adults }}</span>
-                    <span v-if="order.capacity.children">
-                        Children:
-                        <span>{{ order.capacity.children }}</span>
-                    </span>
-                    <span v-if="order.capacity.infants">
-                        Infants:
-                        <span>{{ order.capacity.infants }}</span>
-                    </span>
-                    <span v-if="order.capacity.pets">
-                        Pets:
-                        <span>{{ order.capacity.pets }}</span>
-                    </span>
-                </div>
-                <hr class="hr" />
-                <div class="pricing-for-order">
-                    <span>Price per night:</span>
-                    <span>{{ getFormattedPrice(this.order.price) }} / night</span>
-                    <span>
-                        {{ getFormattedPrice(this.order.price) }}
-                        <span>X</span>
-                        <span>{{ getNumOfNights }} nights</span>
-                    </span>
-
-                    <span>Fees:</span>
-                    <span>{{ getFormattedPrice(this.order.fees) }}</span>
+    <section class="section-row">
+        <section class="order-details-container section-col" v-if="order && isApproved">
+            <div class="order-details-success">
+                <h1>Your Order Details</h1>
+                <p>We thank you, for your reservation! Here are the details about your order:</p>
+                <div class="order-details-desc">
+                    <div class="stay-order-details">
+                        <span>Name of the stay:</span>
+                        <span>{{ order.nameOfStay }}</span>
+                        <span>Address of the stay:</span>
+                        <span>{{ order.location }}</span>
+                        <span>The host:</span>
+                        <span>{{ order.host }}</span>
+                    </div>
                     <hr class="hr" />
-                    <span>
-                        Total:
-                        <span>{{ getTotal }}</span>
-                    </span>
+                    <span>From:</span>
+                    <span>{{ getFormattedDate(order.dates['0']) }}</span>
+                    <span>To:</span>
+                    <span>{{ getFormattedDate(order.dates['1']) }}</span>
+                    <hr class="hr" />
+                    <div class="guests-order-details">
+                        <span>Number of guests:</span>
+                        <span>Adults:</span>
+                        <span>{{ order.capacity.adults }}</span>
+                        <span v-if="order.capacity.children">
+                            Children:
+                            <span>{{ order.capacity.children }}</span>
+                        </span>
+                        <span v-if="order.capacity.infants">
+                            Infants:
+                            <span>{{ order.capacity.infants }}</span>
+                        </span>
+                        <span v-if="order.capacity.pets">
+                            Pets:
+                            <span>{{ order.capacity.pets }}</span>
+                        </span>
+                    </div>
+                    <hr class="hr" />
+                    <div class="pricing-for-order">
+                        <span>Price per night:</span>
+                        <span>{{ getFormattedPrice(this.order.price) }} / night</span>
+                        <span>
+                            {{ getFormattedPrice(this.order.price) }}
+                            <span>X</span>
+                            <span>{{ getNumOfNights }} nights</span>
+                        </span>
+
+                        <span>Fees:</span>
+                        <span>{{ getFormattedPrice(this.order.fees) }}</span>
+                        <hr class="hr" />
+                        <span>
+                            Total:
+                            <span>{{ getTotal }}</span>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <section v-if="!order"></section>
-    <!-- <easy-spinner /> -->
-    <section v-if="order && !isApproved">
-        <h2>Reservation Status: Pending</h2>
-        <h3>Reservation details</h3>
-        <ul>
-            <li>
-                Stay name: {{ order.nameOfStay }}
-            </li>
-            <li>
-                Stay address: {{ order.location }}
-            </li>
-            <li>
-                The host: {{ order.host }}
-            </li>
-            <li>
-                {{ getFormattedDate(order.dates['0']) }}
-            </li>
-            <li>
-                {{ getFormattedDate(order.dates['1']) }}
-            </li>
-            <li>
-                Number of guests:
-                <span>Adults: {{ order.capacity.adults }}</span>
-                <span v-if="order.capacity.children">
-                    Children: {{ order.capacity.children }}
-                </span>
-                <span v-if="order.capacity.infants">
-                    Infants: {{ order.capacity.infants }}
-                </span>
-                <span v-if="order.capacity.pets">
-                    Pets: {{ order.capacity.pets }}
-                </span>
-            </li>
-            <li>
-                <span>Price per night (per person): {{ getFormattedPrice(this.order.price) }}</span>
-            </li>
-            <li>
-                Number of nights: {{ getNumOfNights }} nights
-            </li>
-            <li>
-                Total price excluding fees: {{ getTotalExFees }}
-            </li>
-            <li>
-            Fees:
-            <span>
-                Cleaning fee: {{ this.order.cleaningFee }}
-            </span>
-            <span>
-                Security deposit: {{ this.order.securityDeposit }}
-            </span>
-            </li>
-            <li>
-                Total price: {{getTotalPrice}}
-            </li>
-
-        </ul>
+        </section>
+        <section v-if="!order"></section>
+        <!-- <easy-spinner /> -->
+        <section class="pending-reservation-container section-col" v-if="order && !isApproved">
+            <div class="pending-status-container">
+                <h2>Reservation Status: Pending</h2>
+                <div class="pending-img">
+                    <img src="src/assets/icons/wall-clock.png" alt />
+                </div>
+            </div>
+            <h3>Reservation details</h3>
+            <ul class="clean-list list-container">
+                <div class="desc-li-container">
+                    <li>
+                        <span class="li-title">Stay name:</span>
+                        <span class="li-content">{{ order.nameOfStay }}</span>
+                    </li>
+                    <li>
+                        <span class="li-title">Stay address:</span>
+                        <span class="li-content">{{ order.location }}</span>
+                    </li>
+                    <li>
+                        <span class="li-title">The host:</span>
+                        <span class="li-content">{{ order.host }}</span>
+                    </li>
+                    <li>
+                        <span class="li-title">From:</span>
+                        <span class="li-content">{{ getFormattedDate(order.dates['0']) }}</span>
+                    </li>
+                    <li>
+                        <span class="li-title">To:</span>
+                        <span class="li-content">{{ getFormattedDate(order.dates['1']) }}</span>
+                    </li>
+                </div>
+                <div class="guests-li-container">
+                    <li>
+                        <span class="li-title li-upper-title">Number of guests:</span>
+                        <div class="guests-box">
+                            <div class="guests-li">
+                                <div class="li-title">Adults:</div>
+                                <div class="li-content">{{ order.capacity.adults }}</div>
+                            </div>
+                            <div v-if="order.capacity.children" class="guests-li">
+                                <div class="li-title">Children:</div>
+                                <div class="li-content">{{ order.capacity.children }}</div>
+                            </div>
+                            <div v-if="order.capacity.infants" class="guests-li">
+                                <div class="li-title">Infants:</div>
+                                <div class="li-content">{{ order.capacity.infants }}</div>
+                            </div>
+                            <div v-if="order.capacity.pets" class="guests-li">
+                                <div class="li-title">Pets:</div>
+                                <div class="li-content">{{ order.capacity.pets }}</div>
+                            </div>
+                        </div>
+                    </li>
+                </div>
+                <div class="price-li-container">
+                    <li>
+                        <span>
+                            <span class="li-title">Price per night (per person):</span>
+                            <span class="li-content">{{ getFormattedPrice(this.order.price) }}</span>
+                        </span>
+                    </li>
+                    <li>
+                        <span class="li-title">Number of nights:</span>
+                        <span class="li-content">{{ getNumOfNights }} nights</span>
+                    </li>
+                    <li>
+                        <span class="li-title">Total price excluding fees:</span>
+                        <span class="li-content">{{ getTotalExFees }}</span>
+                    </li>
+                    <li>
+                        <span class="li-title li-upper-title">Fees:</span>
+                        <div class="fees-box">
+                            <div class="cleaning-fee-container">
+                                <div class="li-title">Cleaning fee:</div>
+                                <div class="li-content">${{ this.order.cleaningFee }}</div>
+                            </div>
+                            <div class="security-fee-container">
+                                <div class="li-title">Security deposit:</div>
+                                <div class="li-content">${{ this.order.securityDeposit }}</div>
+                            </div>
+                        </div>
+                    </li>
+                </div>
+                <hr class="hr" />
+                <li>
+                    <span class="li-title">Total price:</span>
+                    <span class="li-content">{{ getTotalPrice }}</span>
+                </li>
+            </ul>
+        </section>
     </section>
 </template>
 
@@ -130,8 +166,8 @@ export default {
         this.order = await this.$store.getters.order
         console.log(this.order);
         setTimeout(() => {
-            this.isApproved = true;
-        }, 60000);
+            this.isApproved = false;
+        }, 60000000000);
         // const { id } = this.$route.params
         // this.stay = await stayService.getById(id)
         // console.log(this.stay);
@@ -181,15 +217,15 @@ export default {
             return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format(price)
         },
         getTotalPrice() {
-             let numNights = this.getNumOfNights
+            let numNights = this.getNumOfNights
             let numGuests = this.order.capacity.adults + this.order.capacity.children
             let totalPriceExFees = numNights * (this.order.price) * numGuests
             let cleaningFee = this.order.cleaningFee
             if (!cleaningFee) cleaningFee = 0
             let securityDeposit = this.order.securityDeposit
-             if (!securityDeposit) securityDeposit = 0
-             let totalPrice = totalPriceExFees + cleaningFee + securityDeposit
-             return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format(totalPrice)
+            if (!securityDeposit) securityDeposit = 0
+            let totalPrice = totalPriceExFees + cleaningFee + securityDeposit
+            return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format(totalPrice)
         }
     },
     unmounted() { },
@@ -231,5 +267,59 @@ export default {
 .pricing-for-order {
     display: flex;
     flex-direction: column;
+}
+
+.pending-reservation-container {
+    min-height: 80vh;
+}
+
+.pending-status-container {
+    display: flex;
+    align-items: center;
+    gap: 12.5px;
+}
+
+.pending-img {
+    padding-top: 5px;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+}
+.pending-img img {
+    width: 24px;
+    height: 24px;
+}
+
+.pending-reservation-container .clean-list {
+    flex-direction: column;
+    margin: 0 auto;
+}
+
+.list-container .li-title {
+    font-family: "Airbnb-Medium";
+    font-size: 16px;
+    padding-right: 5px;
+}
+
+.guests-box {
+    display: flex;
+    flex-direction: column;
+}
+.guests-box .guests-li {
+    display: flex;
+    flex-direction: row;
+}
+.fees-box {
+    display: flex;
+    flex-direction: column;
+}
+
+.fees-box .cleaning-fee-container {
+    display: flex;
+    flex-direction: row;
+}
+.fees-box .security-fee-container {
+    display: flex;
+    flex-direction: row;
 }
 </style>
